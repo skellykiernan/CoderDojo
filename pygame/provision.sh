1f1cd8866be84834b9986bad3ca4b743d432b65b
Kelly <skelly@example.com>
2015-01-20 16:49:11 +0000

    Added pycharm download to provisioning Updated Vagrantfile to configure virtual box for 2G of memory Updated Vagrantfile to a common hostname and virtualbox name

--- pygame/provision.sh
+++ pygame/provision.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+##############################################################################
+# Configurable used in the provisioning
+##############################################################################
+# Versions and info for Packages and Tools
+PYCHARM_VERSION='4.0.4'
+PYCHARM_DOWNLOAD_URL='http://download-cf.jetbrains.com/python'
+PYCHARM_PKG_NAME=pycharm-community-${PYCHARM_VERSION}
 
+##############################################################################
+# Install Ubuntu packages
+##############################################################################
 # needed for FFMEG a pygame dependency
 sudo add-apt-repository ppa:jon-severinsson/ffmpeg
 
@@ -23,13 +33,13 @@ check_install_pkg gnome-terminal
 
 # packages required to compile pygame 
 # see http://www.pygame.org/wiki/CompileUbuntu
-declare -a pygame_dep_pkgs=("mercurial"           "python3-dev" 
-                            "python3-numpy"       "ffmpeg"
-                            "libsdl-image1.2-dev" "libsdl-mixer1.2-dev"
-                            "libsdl-ttf2.0-dev"   "libsmpeg-dev" 
-                            "libsdl1.2-dev"       "libportmidi-dev"
-                            "libswscale-dev"      "libavformat-dev"
-                            "libavcodec-dev")
+declare -a pygame_dep_pkgs=("mercurial"           'python3-dev' 
+                            'python3-numpy'       'ffmpeg'
+                            'libsdl-image1.2-dev' 'libsdl-mixer1.2-dev'
+                            'libsdl-ttf2.0-dev'   'libsmpeg-dev' 
+                            'libsdl1.2-dev'       'libportmidi-dev'
+                            'libswscale-dev'      'libavformat-dev'
+                            'libavcodec-dev')
 
 ## now loop through the above array
 for pkg in "${pygame_dep_pkgs[@]}"
@@ -38,8 +48,8 @@ do
 done
 
 # virtualbox guest installations(mainly to improve the screen resolution)
-declare -a virtualbox_pkgs=("virtualbox-guest-dkms" "virtualbox-guest-utils"
-                            "virtualbox-guest-x11")
+declare -a virtualbox_pkgs=('virtualbox-guest-dkms' 'virtualbox-guest-utils'
+                            'virtualbox-guest-x11')
 
 for pkg in "${virtualbox_pkgs[@]}"
 do
@@ -47,10 +57,12 @@ do
 done
 
 # download and compile pygame
-if [[ -d pygame ]]; then
-    echo "++++ Have already download pygame"
+if [[ -d tools/pygame ]]; then
+    echo '++++ Have already download pygame'
 else
-    echo "---- Downloading, build and install pygame"
+    echo '---- Downloading, build and install pygame'
+    mkdir tools
+    cd tools
     # Grab source
     hg clone https://bitbucket.org/pygame/pygame
      
@@ -58,7 +70,33 @@ else
     cd pygame
     python3 setup.py build
     python3 setup.py install
+    cd ../..
 fi    
 
+##############################################################################
+# install development tools
+##############################################################################
+#
 # add python idle 
 check_install_pkg idle3
+
+# java needed for pycharm
+check_install_pkg openjdk-7-jre
+
+# download and unpack PyCharm
+# NOTE: will still need to run the pycharm.sh script in unpacked pycharm bin
+# directory, think that need to use sudo when starting 
+if [[ -d tools/pycharm ]]; then
+    echo '++++ Have already downloaded PyCharm'
+else
+    echo "---- Download and unpack ${PYCHARM_PKG_NAME}"
+    mkdir -p tools/pycharm
+    cd tools/pycharm
+    # downloads the tar.gz linux install file
+    wget ${PYCHARM_DOWNLOAD_URL}/${PYCHARM_PKG_NAME}.tar.gz
+    #wget http://download-cf.jetbrains.com/python/pycharm-community-4.0.4.tar.gz
+    tar xzvf ${PYCHARM_PKG_NAME}.tar.gz
+    rm ${PYCHARM_PKG_NAME}.tar.gz
+    cd ..
+fi
+
