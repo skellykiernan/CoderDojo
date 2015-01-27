7f4310c3e2d14632c25d6010647a45afae1bc776
Skelly Kiernan <skelly@example.com>
2015-01-27 00:43:21 +0000

    Split print to prevent too wide a line

--- pygame/src/question/hello_you.py
+++ pygame/src/question/hello_you.py
@@ -2,4 +2,5 @@ print('What is your name? ')
 name = input()
 greeting = 'Hello ' + name
 print(greeting)
-print('"name" refers to a', type(name), 'that contains', name)
+print('"name" refers to a', type(name), end=' ')
+print('that contains', name)
